7c7cbc477a2ada45446278dad4833fcda6815d5f
kszym2002 <kszym2002@gmail.com>
2020-12-31 22:05:57 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -294,15 +294,10 @@ export default {
           {
             label: "ACL4SSR",
             options: [
-	     {
+              {
                 label: "游戏规则（Eason提供）",
                 value:
                   "https://raw.githubusercontent.com/kszym2002/Merlin_SS/main/game.ini"
-              },
-              {
-                label: "ACL4SSR_Online 默认版 分组比较全(与Github同步)",
-                value:
-                  "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"
               },
               {
                 label: "ACL4SSR_Online_AdblockPlus 更多去广告(与Github同步)",
